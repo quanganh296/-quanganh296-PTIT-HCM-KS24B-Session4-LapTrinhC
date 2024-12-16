2a906b6700beead73bf794f8965f8aeed3350b3c
Quater <102786860+quanganh296@users.noreply.github.com>
2024-12-16 21:15:37 +0700

    Add files via upload

--- KiemTraNamNhuan.cpp
+++ KiemTraNamNhuan.cpp
@@ -1,12 +1,16 @@
 #include<stdio.h>
 using namespace std;
 int main(){
-	int a,b,c;
-	printf("Hay nhap 3 so nguyen:");
-	scanf("%d %d %d",a,b,c);
-	if((a>b&&a>c)&&(b<a&&b>c)&&(c<a&&c<b))
-	{
-	}
+	int year;
+	printf("Hay nhap nam bat ki:");
+	scanf("%d",&month);
+if((month%4==0&&month%100!=0)||(month%100==0&&month%400==0))
+{
+	printf("nam %d la nam nhuan",month);
+}
+else{
+		printf("nam %d ko la nam nhuan",month);
+}
 	
 	return 0;
 }
